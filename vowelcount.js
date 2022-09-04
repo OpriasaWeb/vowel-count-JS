497aaa90ef049d7376245f5bc8421183fe6e6c0c
Jeremy Opriasa <opriasajbusiness@gmail.com>
2022-09-05 00:16:58 +0800

    Vowel count is running well

--- vowelcount.js
+++ vowelcount.js
@@ -1,1 +1,28 @@
-hello
+// hello
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// Jeremy Opriasa - coding practice - VOWEL COUNT
+// Nested loops
+
+function getCount(str){
+  var vowelCount = 0;
+
+  var arrVowel = ['a', 'e', 'i', 'o', 'u'];
+
+  for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < arrVowel.length; j++){
+      if(str[i] === arrVowel[j]){
+        vowelCount = vowelCount + 1;
+      }
+    }
+  }
+
+  return vowelCount;
+}
+
+console.log(getCount('abrakadabrabruhlars'));
